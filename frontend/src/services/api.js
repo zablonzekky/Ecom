b29133f89debdccc24ce55cbe3dadbe31cfe4519
zablonzekky <127195087+zablonzekky@users.noreply.github.com>
2026-02-27 10:13:47 +0300

    Update API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 
-const API_BASE_URL = "http://localhost:8000/api";
+// const API_BASE_URL = "http://localhost:8000/api";
 
+const API_BASE_URL = "https://ecom-426a.onrender.com//api";
 // Function to refresh the access token
 async function refreshAccessToken() {
   const refreshToken = localStorage.getItem('refresh_token');
